8bcb0dc357b58322291dc4e32db8e51bc6d9ff21
Amit Chandi <chandi.amit97@gmail.com>
2025-01-21 01:15:24 -0800

    + incomplete ReplacePostProcess

---
+++ Chandiman.HttpServer/ReplacePostProcess.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Chandiman.HttpServer;
+public class ReplacePostProcess
+{
+    const string ReplaceTag = @"<\?(\s).*(Replace.*=.*"".*"").*\?>";
+    const string ReplaceKV = @"Replace.*=.*"".*""";
+    const string TitleKV = @"Title.*=.*"".*""";
+    /// <summary>
+    /// something something darkside
+    /// </summary>
+    public static string Process(Session session, Dictionary<string, object?> kvParms, string html)
+    {
+        RegexOptions options = RegexOptions.Multiline | RegexOptions.IgnoreCase;
+
+        foreach (Match m in Regex.Matches(html, ReplaceTag, options))
+        {
+            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+            var titleMatch = Regex.Match(m.Value.Substring(m.Index, m.Length), TitleKV, options);
+            if (titleMatch != null)
+            {
+
+            }
+        }
+
+        string ret = "";
+
+        return ret;
+    }
+}
